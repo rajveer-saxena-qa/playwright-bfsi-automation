a6d1763f31e6e5107b6659d718a7146a5a587af4
Rajveer Saxena <rajveersaxena11@yahoo.com>
2026-04-20 13:03:38 +0000

    TC001 - Login positive smoke test passing with env variables and dynamic assertions

--- tests/regression/tc-login-negative-regression.spec.js
+++ tests/regression/tc-login-negative-regression.spec.js
@@ -1,7 +1,5 @@
 const { test, expect } = require('@playwright/test');
-require('dotenv').config();
-const validUID = process.env.GURU99_USERNAME;
-const validPWD = process.env.GURU99_PASSWORD;
+
 const invalidCredentials = [
   { uid: 'invalid123', pwd: 'invalid123', scenario: 'TC002 - Invalid UserID and Invalid Password' },
   { uid: 'validUID', pwd: 'wrongpass', scenario: 'TC003 - Valid UserID and Invalid Password' },
